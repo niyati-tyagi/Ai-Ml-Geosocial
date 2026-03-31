81206747a92f9a410826293e12bf6225c08437c4
niyati-tyagi <niyati.25bce11103@vitbhopal.ac.in>
2026-03-31 20:09:47 +0530

    Create vite-env.d.ts

---
+++ src/vite-env.d.ts
@@ -0,0 +1,22 @@
+/// <reference types="vite/client" />
+/// <reference types="vite-plugin-pwa/client" />
+
+declare module "*.png" {
+  const value: string;
+  export default value;
+}
+
+declare module "*.jpg" {
+  const value: string;
+  export default value;
+}
+
+declare module "*.svg" {
+  const value: string;
+  export default value;
+}
+
+declare module "*.json" {
+  const value: any;
+  export default value;
+}
